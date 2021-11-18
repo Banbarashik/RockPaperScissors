53fa0fa9313ab37beb9653acc3b644f337ae6482
Banbarashik <odinokyskitalec@yahoo.com>
2021-11-18 23:52:24 +0700

    Added GUI to an RPS game

--- script.js
+++ script.js
@@ -1,38 +1,72 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
   let arr = ["rock", "paper", "scissors"];
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-// initializing one round of a game
 function playRound(playerSelection, computerSelection) {
   console.log(computerSelection); // testing computer's input
-  playerSelection = playerSelection.toLowerCase();
+  let result = document.querySelector("#result");
+  let score = document.querySelector("#score");
   if (playerSelection === computerSelection) {
-    return "It's a tie";
+    result.textContent = "It's a tie";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You lose! Paper beats Rock";
+    computerScore++;
+    result.textContent = "You lose! Paper beats Rock";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You won! Rock beats Scissors";
+    playerScore++;
+    result.textContent = "You won! Rock beats Scissors";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You won! Paper beats Rock";
+    playerScore++;
+    result.textContent = "You won! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You lose! Scissors beat Paper";
+    computerScore++;
+    result.textContent = "You lose! Scissors beat Paper";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You lose! Rock beats Scissors";
+    computerScore++;
+    result.textContent = "You lose! Rock beats Scissors";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You won! Scissors beat Paper";
-  } else {
-    return "You made a mistake";
+    playerScore++;
+    result.textContent = "You won! Scissors beat Paper";
+  }
+  score.textContent = `${playerScore} : ${computerScore}`;
+
+  if (playerScore >= 5) {
+    result.textContent = "You won!";
+    document
+      .querySelectorAll("button")
+      .forEach((button) => (button.disabled = true));
+    startAgain();
+  } else if (computerScore >= 5) {
+    result.textContent = "You lose!";
+    document
+      .querySelectorAll("button")
+      .forEach((button) => (button.disabled = true));
+    startAgain();
   }
 }
 
-// 5 rounds of a game
-function game(numOfRounds) {
-  for (i = 0; i < numOfRounds; i++) {
-    const playerInput = prompt("Player's selection");
-    console.log(playRound(playerInput, computerPlay()));
-  }
-  return "The game is over";
+function startAgain() {
+  let btnStartAgain = document.createElement("button");
+  btnStartAgain.textContent = "Start again";
+  document.querySelector(".game-area").appendChild(btnStartAgain);
+  btnStartAgain.addEventListener("click", () => {
+    playerScore = 0;
+    computerScore = 0;
+    result.textContent = "";
+    score.textContent = `${playerScore} : ${computerScore}`;
+    document
+      .querySelectorAll("button")
+      .forEach((button) => (button.disabled = false));
+    btnStartAgain.style.display = "none";
+  });
 }
 
-console.log(game(3));
+const buttons = document.querySelectorAll(`button`);
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playRound(button.value, computerPlay());
+  });
+});
